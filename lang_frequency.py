7a3df33e860f6fe6ca5de6aea378f78b45ebff6b
lucky <lucky@lucky.ilbirs.com>
2017-09-15 00:27:49 +0300

    version 1.02, pep8 fixes

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,13 +2,16 @@ import sys
 import re
 from collections import Counter
 
+
 def load_data(filepath):
     with open(filepath, 'r') as f:
         words = re.findall(r'\w+', f.read().lower())
         return words
 
+
 def get_most_frequent_words(words, top_count):
     return Counter(words).most_common(top_count)
+
 
 if __name__ == '__main__':
     words = load_data(sys.argv[1])
