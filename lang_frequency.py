e021a74376bbec8372caab037fcbce491f5135fd
lucky <lucky@lucky.ilbirs.com>
2017-09-14 21:46:00 +0300

    first version

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,18 @@
+import sys
+import re
 
 
 def load_data(filepath):
-    pass
+    words = {}
+    with open(filepath, 'r') as f:
+        for line in f:
+            """
+            Read line by line to avoid memory issues
+            """
+            for word in re.findall('[\w\d]+', line):
+                words[word] = words[word] + 1 if word in words else 1
 
-
-def get_most_frequent_words(text):
-    pass
-
+        return sorted(words.items(), key=lambda x: x[1], reverse=True)
 
 if __name__ == '__main__':
-    pass
+    print(load_data(sys.argv[1])[:10])
